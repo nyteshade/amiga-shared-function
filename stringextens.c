b45162ddd43a48941400cd475c62d8b730ae5b87
Brielle Harrison <nyteshade@gmail.com>
2020-11-05 10:56:20 -0800

    Update stringextens.c
    Address spacing.

--- stringextens.c
+++ stringextens.c
@@ -4,13 +4,14 @@
 #ifndef __SASC
 
 /* function strlwr() */
-char *strlwr(char *str)
-{
+char *strlwr(char *str) {
   unsigned char *p = (unsigned char *)str;
+  
   while (*p) {
     *p = tolower((unsigned char)*p);
     p++;
   }
+  
   return str;
 }
 
@@ -18,14 +19,16 @@ char *strlwr(char *str)
 char *strrev(char *str)
 {
   char *p1, *p2;
+  
   if (! str || ! *str)
-  return str;
-  for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2)
-  {
+    return str;
+  
+  for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2) {
     *p1 ^= *p2;
     *p2 ^= *p1;
     *p1 ^= *p2;
   }
+  
   return str;
 }
 
@@ -34,18 +37,21 @@ char *strrev(char *str)
 char *strduplwr(char *str, unsigned long requirements)
 {
   unsigned char *p = AllocVec(strlen(str) + 1, requirements);
-  if (!p) {
+  
+  if (!p)
     return NULL;
-  }
+  
   strcpy(p, (unsigned char *)str);
+  
   return (char *)strlwr(p);
 }
 
 char *strduprev(char *str, unsigned long requirements) {
   unsigned char *p = AllocVec(strlen(str) + 1, requirements);
-  if (!p) {
+  
+  if (!p)
     return NULL;
-  }
+  
   strcpy(p, (unsigned char *)str);
   return (char *)strrev(p);
 }
